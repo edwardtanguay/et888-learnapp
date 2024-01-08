b9a4ee35675baa691784b36fccabc9aaf6aecad6
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-08 09:51:39 +0100

    frontend base in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
+			<h1 className="text-3xl mb-3 text-slate-800">LearnApp</h1>
 			<Nav/>
 		</>
 	);
