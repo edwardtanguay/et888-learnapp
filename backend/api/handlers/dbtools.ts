6a25ae2cd54575add03522201fdce93faed11308
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-08 15:03:02 +0100

    suuid is created

--- backend/api/handlers/dbtools.ts
+++ backend/api/handlers/dbtools.ts
@@ -13,5 +13,14 @@ export const getDb = async () => {
 }
 
 export const getSuuid = () => {
-	return 'aaaaaa';
+	const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	const length = 6;
+	let suuid = '';
+
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		suuid += characters.charAt(randomIndex);
+	}
+
+	return suuid;
 }
