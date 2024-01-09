b1da00dbd1b4cb453d4c807265519e6b16c1ae70
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 10:35:42 +0100

    flashcards load

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { flashcardRouter } from './routers/flashcardrouter';
+import cors from 'cors';
 
 export const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.json({
--- src/pages/PageInfo.tsx
+++ src/pages/PageInfo.tsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
 export const PageInfo = () => {
-	
+	const { flashcards } = useContext(AppContext);
 	return (
-		<p>This is the info page.</p>
+		<p>This is the info page with {flashcards.length} flashcards</p>
 	)
 }
