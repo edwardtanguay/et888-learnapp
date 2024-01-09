2b0ed327cc2f50095852396e5d3136a45201c1a3
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 10:54:50 +0100

    mock table data

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -4,6 +4,31 @@ import { AppContext } from "../AppContext";
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
 	return (
+		<>
 		<p>There are {flashcards.length} flashcards:</p>
+
+		<table>
+				<thead>
+					<tr>
+						<th>SUUID</th>
+						<th>Category</th>
+						<th>Front</th>
+						<th>Back</th>
+					</tr>
+				</thead>
+				<tbody>
+					{flashcards.map(flashcard => {
+						return (
+							<tr>
+								<td>nnn</td>
+								<td>nnn</td>
+								<td>nnn</td>
+								<td>nnn</td>
+							</tr>
+						)
+					})}
+				</tbody>
+		</table>
+		</>
 	)
 }
