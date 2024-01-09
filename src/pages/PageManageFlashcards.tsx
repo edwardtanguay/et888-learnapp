1c3b188f6d452aedfe7eff34b4b946b7091de252
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 11:18:40 +0100

    react-icons for crud in

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -1,34 +1,48 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import { MdModeEditOutline } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { SiOneplus } from "react-icons/si";
 
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
 	return (
 		<>
-		<p>There are {flashcards.length} flashcards:</p>
+			<p>There are {flashcards.length} flashcards:</p>
 
-		<table className="dataTable mt-4">
+			<table className="dataTable mt-4">
 				<thead>
 					<tr>
 						<th>SUUID</th>
 						<th>Category</th>
 						<th>Front</th>
 						<th>Back</th>
+						<th>
+							<div className="flex justify-center text-[#222] text-2xl">
+								<SiOneplus />
+							</div>
+						</th>
 					</tr>
 				</thead>
 				<tbody>
-					{flashcards.map(flashcard => {
+					{flashcards.map((flashcard) => {
 						return (
 							<tr key={flashcard.suuid}>
 								<td>{flashcard.suuid}</td>
 								<td>{flashcard.category}</td>
 								<td>{flashcard.front}</td>
 								<td>{flashcard.back}</td>
+								<td>
+									<div className="flex gap-1">
+										<MdModeEditOutline />
+										<RiDeleteBin6Line />
+									</div>
+								</td>
 							</tr>
-						)
+						);
 					})}
 				</tbody>
-		</table>
+			</table>
 		</>
-	)
-}
+	);
+};
