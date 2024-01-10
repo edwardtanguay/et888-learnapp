b34a7a65b307740f036a135b468a804f222dd7d5
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-10 15:18:14 +0100

    main row refactored in component

---
+++ src/components/FlashcardTableMainRow.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MdCancel, MdModeEditOutline } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { IFrontendFlashcard, convertFrontendFlashcardToFlaschard } from "../shared/interfaces";
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
+interface IProps {
+	frontendFlashcard: IFrontendFlashcard;
+}
+
+export const FlashcardTableMainRow = ({ frontendFlashcard }: IProps) => {
+	const {
+		deleteFlashcard,
+		setFrontendFlashcards,
+		frontendFlashcards
+	} = useContext(AppContext);
+	
+	const handleDeleteFlashcard = (frontendFlashcard: IFrontendFlashcard) => {
+		(async () => {
+			try {
+				const flashcard =
+					convertFrontendFlashcardToFlaschard(frontendFlashcard);
+				await deleteFlashcard(flashcard);
+			} catch (e: any) {
+				console.log(`${e.message}`);
+				alert(
+					"We're sorry, your flashcard cannot be saved at this time. Try again later, or contact 2342-234-23343."
+				);
+			}
+		})();
+	};
+	
+	const handleSetFlashcardToDeleting = (
+		frontendFlashcard: IFrontendFlashcard
+	) => {
+		frontendFlashcard.userIsDeleting = !frontendFlashcard.userIsDeleting;
+		setFrontendFlashcards(structuredClone(frontendFlashcards));
+	};
+
+	return (
+		<tr
+			className={frontendFlashcard.userIsDeleting ? "deleting" : ""}
+			key={frontendFlashcard.suuid}
+		>
+			<td>{frontendFlashcard.suuid}</td>
+			<td>{frontendFlashcard.category}</td>
+			<td>{frontendFlashcard.front}</td>
+			<td>{frontendFlashcard.back}</td>
+			<td>
+				{frontendFlashcard.userIsDeleting ? (
+					<div className="flex gap-1">
+						<RiDeleteBin6Line
+							onClick={() =>
+								handleDeleteFlashcard(frontendFlashcard)
+							}
+							className="cursor-pointer hover:text-red-900"
+						/>
+						<MdCancel
+							onClick={() =>
+								handleSetFlashcardToDeleting(frontendFlashcard)
+							}
+							className="cursor-pointer hover:text-red-900"
+						/>
+					</div>
+				) : (
+					<div className="flex gap-1">
+						<MdModeEditOutline className="cursor-pointer hover:text-green-900" />
+						<RiDeleteBin6Line
+							onClick={() =>
+								handleSetFlashcardToDeleting(frontendFlashcard)
+							}
+							className="cursor-pointer hover:text-red-900"
+						/>
+					</div>
+				)}
+			</td>
+		</tr>
+	);
+};
--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -1,44 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useContext, useState } from "react";
 import { AppContext } from "../AppContext";
-import { MdModeEditOutline, MdCancel } from "react-icons/md";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import {
-	IFrontendFlashcard,
-	convertFrontendFlashcardToFlaschard,
-} from "../shared/interfaces";
 import { FlashcardTableHead } from "../components/FlashcardTableHead";
 import { FlashcardTableAddRow } from "../components/FlashcardTableAddRow";
+import { FlashcardTableMainRow } from "../components/FlashcardTableMainRow";
 
 export const PageManageFlashcards = () => {
 	const {
 		frontendFlashcards,
-		setFrontendFlashcards,
-		deleteFlashcard,
 	} = useContext(AppContext);
 	const [isAddingFlashcard, setIsAddingFlashcard] = useState(false);
-
-	const handleSetFlashcardToDeleting = (
-		frontendFlashcard: IFrontendFlashcard
-	) => {
-		frontendFlashcard.userIsDeleting = !frontendFlashcard.userIsDeleting;
-		setFrontendFlashcards(structuredClone(frontendFlashcards));
-	};
-
-	const handleDeleteFlashcard = (frontendFlashcard: IFrontendFlashcard) => {
-		(async () => {
-			try {
-				const flashcard =
-					convertFrontendFlashcardToFlaschard(frontendFlashcard);
-				await deleteFlashcard(flashcard);
-			} catch (e: any) {
-				console.log(`${e.message}`);
-				alert(
-					"We're sorry, your flashcard cannot be saved at this time. Try again later, or contact 2342-234-23343."
-				);
-			}
-		})();
-	};
 
 	return (
 		<>
@@ -54,53 +25,7 @@ export const PageManageFlashcards = () => {
 						{isAddingFlashcard && <FlashcardTableAddRow setIsAddingFlashcard={setIsAddingFlashcard} />}
 						{frontendFlashcards.map((frontendFlashcard) => {
 							return (
-								<tr
-									className={
-										frontendFlashcard.userIsDeleting
-											? "deleting"
-											: ""
-									}
-									key={frontendFlashcard.suuid}
-								>
-									<td>{frontendFlashcard.suuid}</td>
-									<td>{frontendFlashcard.category}</td>
-									<td>{frontendFlashcard.front}</td>
-									<td>{frontendFlashcard.back}</td>
-									<td>
-										{frontendFlashcard.userIsDeleting ? (
-											<div className="flex gap-1">
-												<RiDeleteBin6Line
-													onClick={() =>
-														handleDeleteFlashcard(
-															frontendFlashcard
-														)
-													}
-													className="cursor-pointer hover:text-red-900"
-												/>
-												<MdCancel
-													onClick={() =>
-														handleSetFlashcardToDeleting(
-															frontendFlashcard
-														)
-													}
-													className="cursor-pointer hover:text-red-900"
-												/>
-											</div>
-										) : (
-											<div className="flex gap-1">
-												<MdModeEditOutline className="cursor-pointer hover:text-green-900" />
-												<RiDeleteBin6Line
-													onClick={() =>
-														handleSetFlashcardToDeleting(
-															frontendFlashcard
-														)
-													}
-													className="cursor-pointer hover:text-red-900"
-												/>
-											</div>
-										)}
-									</td>
-								</tr>
+								<FlashcardTableMainRow frontendFlashcard={frontendFlashcard}/>
 							);
 						})}
 					</tbody>
