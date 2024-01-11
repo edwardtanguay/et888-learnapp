806361bfcbb042c5365075ccec0502480a3b45a9
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-11 11:41:25 +0100

    showing number of flashcards

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -1,6 +1,10 @@
+import db from '../../backend/data/db.json';
+
+const flashcards = db.flashcards;
+
 export const PageFlashcards = () => {
 
 	return (
-		<p>This is the flashcards page.</p>
+		<p>There are {flashcards.length} flashcards:</p>
 	)
 }
